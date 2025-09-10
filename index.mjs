307174f6a40fb9a8d53af1d5910e9ac152f0994c
Carlos Palacio <carlos.palacio@ikasle.aeg.eus>
2025-09-10 10:02:02 +0200

    donuts con mas calories

--- index.mjs
+++ index.mjs
@@ -25,20 +25,20 @@ function donutWithMostSugar()
 
     }
 
-    console.log("sugars: " + donutWithMostSugar.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars)
+    // console.log("sugars: " + donutWithMostSugar.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars)
     return donutWithMostSugar;
 
 }
-// console.log(donuts)
-// console.log(maxSugarinDonut)
 
+console.log("")
 let realdonutMaxSugar = donutWithMostSugar();
-// console.log(realdonutMaxSugar)
-// console.log("Donut's Name: " + realdonutMaxSugar.name)
+console.log("Donut's with highes sugars: " + realdonutMaxSugar.name + " With " + realdonutMaxSugar.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.sugars)
 
-console.log(donuts[0].nutrition_facts.nutrition.vitamins)
+console.log("")
 
-function findIronInDonut ()
+// console.log(donuts[0].nutrition_facts.nutrition.vitamins)
+
+function donutWithMostIron ()
 {
     let donutWithMostIron = null;
 
@@ -56,26 +56,116 @@ function findIronInDonut ()
 
                 let ironPercent = parseInt(donutVitamin[j].percent)
 
-                console.log(ironPercent)
+                // console.log(ironPercent)
 
                 if(maxIron < ironPercent)
                 {
                     maxIron = ironPercent;
 
-                    console.log(maxIron)
+                    donutWithMostIron = donuts[i]
 
-                    donutWithMostIron = donutVitamin[j]
-
-                    console.log(donutVitamin[j])
                 }
          
             }
         }
         
     }
+    // console.log(maxIron)
 
+
+    return donutWithMostIron;
 }
-let ironInDonut = findIronInDonut()
+let donutWithMaxIron = donutWithMostIron()
+
+console.log("Donut with great amounts of Iron: " + donutWithMaxIron.name)
+
+console.log(donutWithMaxIron.nutrition_facts.nutrition.vitamins)
+
+//donut con más proteína (+ 50 exp)
+
+function findDonutWithMostProtein ()
+{
+    let donutWithMostProtein = null;
+    
+    let maxProtein = 0;
+
+    for (let i = 0; i < donuts.length; ++i)
+    {
+
+        let proteinNumbers = parseInt(donuts[i].nutrition_facts.nutrition.protein)
+
+
+        if(maxProtein < proteinNumbers)
+        {
+            maxProtein = proteinNumbers;
+
+            donutWithMostProtein = donuts[i]
+        }
+    }
+    return donutWithMostProtein;
+}
+
+let donutWithMostProtein = findDonutWithMostProtein();
+
+console.log(" ")
+
+console.log("Donut with most Protein: " + donutWithMostProtein.name + " With " + donutWithMostProtein.nutrition_facts.nutrition.protein)
+
+//donut con menos fibra (+ 50 exp)
+
+function findTheMostFibre()
+{
+    let donutWIthMostFibre = null;
+    
+    let maxFibre = 0;
+
+    for (let i = 0; i < donuts.length; ++i)
+    {
+        let fibreNumbers = parseInt(donuts[i].nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre)
+
+        if (maxFibre < fibreNumbers)
+        {
+            maxFibre = fibreNumbers;
+
+            donutWIthMostFibre = donuts[i];
+        }
+    }
+    return donutWIthMostFibre;
+}
+
+console.log(" ")
+
+let donutWithMostFibre = findTheMostFibre()
+
+console.log("Donut With Most Fibre: " + donutWithMostFibre.name + " With " + donutWithMostFibre.nutrition_facts.nutrition.carbohydrate.carbs_detail.type.fibre)
+
+//donut con más calorías (+ 50 exp)
+
+
+function findDonutWithMostCal ()
+{
+    let donutWithMostCal = null;
+
+    let maxCalFound = 0;
+     
+    for (let i = 0; i < donuts.length; ++i)
+    {
+        if (maxCalFound < donuts[i].nutrition_facts.nutrition.calories)
+        {
+            maxCalFound = donuts[i].nutrition_facts.nutrition.calories;
+
+            donutWithMostCal = donuts[i]
+        }
+    }
+    return donutWithMostCal;
+}
+
+let donutWithMostCal = findDonutWithMostCal();
+
+console.log(" ");
+console.log("Donut with highest calories: " + donutWithMostCal.name + " With " + donutWithMostCal.nutrition_facts.nutrition.calories)
+
+
 //2
 
 
